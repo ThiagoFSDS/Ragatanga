e567261abb4690215c061ed595c290bd4f6528bd
ThiagoFSDS <thiago.f.s.sanson@gmail.com>
2025-10-22 16:11:49 -0300

    firebase atualizada

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6UH-uuPmSSpgm169z56SpgWQWXmBDLC0",
-  authDomain: "hackaton-13f8f.firebaseapp.com",
-  projectId: "hackaton-13f8f",
-  storageBucket: "hackaton-13f8f.firebasestorage.app",
-  messagingSenderId: "762918522059",
-  appId: "1:762918522059:web:062f3a94f4daf5c6595f4e",
-  measurementId: "G-XZB6ZDSSCF"
-};
+    apiKey: "AIzaSyB6UH-uuPmSSpgm169z56SpgWQWXmBDLC0",
+    authDomain: "hackaton-13f8f.firebaseapp.com",
+    projectId: "hackaton-13f8f",
+    storageBucket: "hackaton-13f8f.firebasestorage.app",
+    messagingSenderId: "762918522059",
+    appId: "1:762918522059:web:062f3a94f4daf5c6595f4e",
+    measurementId: "G-XZB6ZDSSCF"
+  };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
